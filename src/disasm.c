7331e08aff0492de0f2f78994a2abb97debe56b4
Elias Lanfranconi <elias.lanfranconi@thameswater.co.uk>
2024-03-12 14:09:30 +0000

    single register cases, mod switch not implemented

--- src/disasm.c
+++ src/disasm.c
@@ -27,15 +27,71 @@ int main(int argc, char **argv)
     Instruction inst = getInstruction(&o);
     free(o.fileData);
 
-    char *asmb = (char *) calloc(4, sizeof(char));
-    switch(inst.opcode)
-    {
-	case 34:
-	    strcpy(asmb, "mov");
-    }
-
     char regs[2][8][3] = {
 	{"AL", "CL", "DL", "BL", "AH", "CH", "DH", "BH"},
 	{"AX", "CX", "DX", "BX", "SP", "BP", "SI", "DI"}
     };
+
+    char *op = (char *) calloc(3, sizeof(char));
+    char *src = (char *) calloc(2, sizeof(char));
+    char *dst = (char *) calloc(2, sizeof(char));
+    
+    switch(inst.opcode) // OBTAINING OPCODE
+    {
+	case 34:
+	    strcpy(op, "mov");
+    }
+    
+    /*
+      this should actually depend on MOD = 11,
+      add it
+    */
+    switch(inst.d) // D GIVES DIRECTION THROUGH REG
+    {
+	case 0: // REG is src
+	{
+	    switch(inst.w)
+	    {
+		case 0: // BYTE
+		{
+		    strcpy(src, regs[0][inst.reg]);
+		    strcpy(dst, regs[0][inst.rm]);
+		    break;
+		}
+
+		case 1: // WORD
+		{
+    		    strcpy(src, regs[1][inst.reg]);
+    		    strcpy(dst, regs[1][inst.rm]);
+		    break;
+		}
+	    }
+	    break;
+	}
+	
+	case 1: // REG is dst
+	{
+	    switch(inst.w)
+	    {
+		case 0:
+		{
+		    strcpy(src, regs[0][inst.rm]);
+		    strcpy(dst, regs[0][inst.reg]);
+		    break;
+		}
+
+		case 1:
+		{
+    		    strcpy(src, regs[1][inst.rm]);
+    		    strcpy(dst, regs[1][inst.reg]);
+		    break;
+		}
+	    }
+	    break;
+	}
+    }
+
+    free(op);
+    free(src);
+    free(dst);
 }
